20ff00912f5e66f7f013acac9c0b22857a6f4808
Uday Verma <uday.karan@gmail.com>
2014-09-05 17:21:26 -0500

    Add a simple private function to get list of nearby users

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // private code //
+
+var geofire = require("geofire");
+var _ = require("lodash");
+
+var randInt = function(x) {
+  return Math.floor(Math.random() * x);
+}
+
+var randOffset = function(x) {
+  return x + Math.random() * 0.000001;
+}
+
+module.exports.findNearBy = function(lat, lng, cb) {
+  // TODO: Add code to actually look people up
+  setTimeout(function() {
+    var users = _.times(randInt(20), function(i) {
+      return {
+        lat: randOffset(lat),
+        lng: randOffset(lng)
+      };
+    });
+
+    cb(null, users);
+  }, cb);
+}
